2c40c0779cf73eb29ee1d077ba304c74bf8d6682
Mynische <620vmanish@gmail.com>
2024-12-27 10:44:57 +0000

    eighth day

---
+++ enums.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+    // enums = a user-defined data type that consists
+    //         of paired named-integer constants.
+    //         Great if you have a set of potential options
+
+    return 0;
+}
